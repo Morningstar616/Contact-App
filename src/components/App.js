e807eeb59bc1b19acfa324dfeef91a1e4ca59b53
Morningstar616 <ashwathjohn2002@gmail.com>
2024-02-11 14:58:57 +0300

    List Rendering and Pass Referencing

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,23 @@ import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const contacts = [
+    {
+      id:"1",
+      name:"John",
+      email:"john23@gmail.com",
+    },
+    {
+      id:"2",
+      name:"Doe",
+      email:"doe14@gmail.com",
+    }
+  ]
   return (
-    <div>
+    <div className='ui container'>
       <Header />
       <AddContact />
-      {/*<ContactCard />
-  <ContactList />*/}
+      <ContactList contacts={contacts} />
     </div> 
   );
 }

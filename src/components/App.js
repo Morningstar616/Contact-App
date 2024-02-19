b61fc3872190598f400a0c3cf319b07ef19c95ae
Morningstar616 <ashwathjohn2002@gmail.com>
2024-02-19 23:31:12 +0300

    Usestate and hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from "./Header";
 import ContactCard from "./ContactCard";
@@ -6,22 +6,16 @@ import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const contacts = [
-    {
-      id:"1",
-      name:"John",
-      email:"john23@gmail.com",
-    },
-    {
-      id:"2",
-      name:"Doe",
-      email:"doe14@gmail.com",
-    }
-  ]
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+  }
+  
   return (
     <div className='ui container'>
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler} />
       <ContactList contacts={contacts} />
     </div> 
   );
